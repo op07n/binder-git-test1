4bf85f564696dc55829fd86a9061d4fe9d9caa4d
op07n <39182105+op07n@users.noreply.github.com>
2020-01-10 11:03:12 +0100

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -4,4 +4,4 @@ import os
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
     os.chdir('simple-streamlit-app')
-    Popen(["streamlit", "run", "simple_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    Popen(["streamlit", "run", "simple_app.py", "--browser.serverAddress=0.0.0.0", "--browser.gatherUsageStats=False", "--server.enableCORS=False"])
